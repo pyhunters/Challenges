32b067f8d91130594c78c4140498aabadcf6ed22
pyhunters <49562437+pyhunters@users.noreply.github.com>
2020-01-13 11:44:50 -0800

    Update challenge1.py

--- Week3/challenge1.py
+++ Week3/challenge1.py
@@ -5,12 +5,6 @@ Try to write program that counts from 0 to 10 using python
 
 '''
 
-#------------------ @start:missx------------------------------
-
-print('count 0')
-
-#-------------------- @end:missx------------------------------
-
 
 
 #------------------ @start:zaid------------------------------
