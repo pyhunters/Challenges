c34201a048da287196e3ca5105ca0e65b000f150
mamdouh <mamdouh.29@gmail.com>
2020-01-25 23:20:58 +0200

    week -4 challenge 1
    Sort string

--- week4/challenge1.py
+++ week4/challenge1.py
@@ -6,7 +6,7 @@ my_list = ['HKU3K','KJI8TE','IIUH1I','I7YUI','OPRE9','5JGU','ER4TB','HY0OH']
 المطلوب ترتيب هذه القائمة تصاعديًا 
 ليكون المخرج كالتالي 
 
-['HY0OH','IIUH1I','HKU3K','ER4TB','5JGU','I7YUI','KJI8TE']
+['HY0OH','IIUH1I','HKU3K','ER4TB','5JGU','I7YUI','KJI8TE','OPRE9']
 
 '''
 #------------------ @start:zaid------------------------------
@@ -73,15 +73,21 @@ my_list = ['HKU3K','KJI8TE','IIUH1I','I7YUI','OPRE9','5JGU','ER4TB','HY0OH']
 
 
 #------------------ @start:ممدوح------------------------------
-
-
-
-
-
-
-
-
-
+my_list = ['HKU3K', 'KJI8TE', 'IIUH1I',
+           'I7YUI', 'OPRE9', '5JGU', 'ER4TB', 'HY0OH']
+x = 0
+d={}
+while x < len(my_list):
+    letter = my_list[x]
+    for p in letter:
+        if p.isnumeric() == True:
+            d[p]=d.get(p,letter)
+    x += 1
+sortd=sorted(d.items(),reverse=False)
+mysorted_list=[]
+for i in sortd:
+	mysorted_list.append(i[1])
+print(mysorted_list)
 
 #-------------------- @end:ممدوح------------------------------
 
