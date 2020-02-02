3e2e038dc99106c333ef2469fccbb7a50cf0bc7f
pyhunters <49562437+pyhunters@users.noreply.github.com>
2020-02-02 09:01:44 -0800

    Create challenge3.py

---
+++ week5/challenge3.py
@@ -0,0 +1,9 @@
+'''
+Write a Python function that takes a list and returns a new list with unique elements of the first list. 
+
+المطلوب عمل دالة تقوم باستقبال قائمة ثم إعادة قائمة تحتوي ع نفس العناصر بدون تكرار 
+
+Sample List : [1,2,3,3,3,3,4,5]
+Unique List : [1, 2, 3, 4, 5]
+
+'''
