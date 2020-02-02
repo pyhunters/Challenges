52efe85a11ea0cbfed2323fc7dbec6eab6ab9919
pyhunters <49562437+pyhunters@users.noreply.github.com>
2020-02-02 09:02:29 -0800

    Create challenge4.py

---
+++ week5/challenge4.py
@@ -0,0 +1,7 @@
+'''
+Write a Python function that checks whether a passed string is palindrome or not. Go to the editor
+Note: A palindrome is a word, phrase, or sequence that reads the same backward as forward, e.g., madam or nurses run.
+
+المطلوب عمل دالة تحدد ما إذا كانت الكلمة المدخلة يمكن قرائتها من البداية للنهاية كما يمكن قرائتها من النهاية للبداية
+
+'''
