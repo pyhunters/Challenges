ee53a5705a12f84b9bfdccad0674150b0e82c86b
pyhunters <49562437+pyhunters@users.noreply.github.com>
2020-02-02 09:07:52 -0800

    Update challenge1.py

--- week5/challenge1.py
+++ week5/challenge1.py
@@ -8,3 +8,47 @@ Expected Output : -336
 '''
 
 
+@zaid 
+
+def mult(x):
+  i=0
+  m=1
+  while i<len(x):
+    m=m*x[i]
+    i+=1
+  print(m)
+L=[1,2,3,4,5,6,7]
+mult(L)
+
+
+@khalid 
+
+list = [1, 2, 3, 4, 5]
+def multi(my_list):
+  variable = 1
+  for item in my_list:
+      multiplication = item * variable
+      car = multiplication
+      return multipiction 
+print(multi(list))
+
+
+@mmdouh
+
+my_list=[8, 2, 3, -1, 7]
+mult=1
+def multiblay(mylist):
+  for i in my_list:
+    mult*=i
+  print(mult)
+  
+ multiblay(my_list)
+
+
+
+
+
+
+
+
+
