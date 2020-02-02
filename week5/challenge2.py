2b9346489d9bfdd7a36151973d242457ce0f98c8
pyhunters <49562437+pyhunters@users.noreply.github.com>
2020-02-02 09:09:10 -0800

    Update challenge2.py

--- week5/challenge2.py
+++ week5/challenge2.py
@@ -6,3 +6,28 @@
 Sample String : "1234abcd"
 Expected Output : "dcba4321"
 '''
+
+@missx 
+
+
+def reverse_str(string):
+  my_list = list(string)
+  my_list.reverse()
+  return ''.join(my_list)
+
+str_ = '1234abcd'
+print(reverse_str(str_))
+#output :dcba4321
+
+
+
+
+def reverse(string):  
+    return string[::-1]
+
+str_ = '1234abcd'
+print(reverse(str_))
+#output :dcba4321
+
+
+
