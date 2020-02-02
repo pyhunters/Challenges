4ef5fffd87a05e1623f30a3fc730b1e0923e04ec
pyhunters <49562437+pyhunters@users.noreply.github.com>
2020-02-02 09:01:05 -0800

    Create challenge2.py

---
+++ week5/challenge2.py
@@ -0,0 +1,8 @@
+'''
+ Write a Python program to reverse a string. 
+
+المطلوب كتابة دالة تستقبل string وتقوم بإعادة ترتيب عناصرها من النهاية للبداية وتقوم بارجاع الشكل الأخير 
+
+Sample String : "1234abcd"
+Expected Output : "dcba4321"
+'''
