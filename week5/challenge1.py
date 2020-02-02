fda7d333db8fd24734103475536ab97d705a98f9
pyhunters <49562437+pyhunters@users.noreply.github.com>
2020-02-02 09:00:27 -0800

    Create challenge1.py

---
+++ week5/challenge1.py
@@ -0,0 +1,10 @@
+'''
+Write a Python function to multiply all the numbers in a list. 
+المطلوب دالة تستقبل قائمة وتقوم بضرب جميع القيم وإعادة ناتج الضرب 
+
+Sample List : [8, 2, 3, -1, 7]
+Expected Output : -336
+
+'''
+
+
