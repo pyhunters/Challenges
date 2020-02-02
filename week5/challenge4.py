6f989fdcfbd873efa8295aef474493878b492d07
pyhunters <49562437+pyhunters@users.noreply.github.com>
2020-02-02 09:11:07 -0800

    Update challenge4.py

--- week5/challenge4.py
+++ week5/challenge4.py
@@ -5,3 +5,11 @@ Note: A palindrome is a word, phrase, or sequence that reads the same backward a
 المطلوب عمل دالة تحدد ما إذا كانت الكلمة المدخلة يمكن قرائتها من البداية للنهاية كما يمكن قرائتها من النهاية للبداية
 
 '''
+
+@missx 
+def palindrome(string):
+  return string == string[::-1]
+
+print(palindrome('خوخ'))
+True
+
