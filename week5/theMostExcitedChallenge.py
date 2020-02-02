bfe8ebbb8088715dfb11a524acecfd6bda1bf7f3
pyhunters <49562437+pyhunters@users.noreply.github.com>
2020-02-02 09:14:05 -0800

    Delete theMostExcitedChallenge.py

--- week5/theMostExcitedChallenge.py
+++
@@ -1,7 +0,0 @@
-'''
-my_list = ['HKU3K','KJI8TE','IIUH1I','I7YUI','OPRE9','5JGU','ER4TB','HY0OH']
-لديكم هذه القائمة تحتوي على عناصر  المطلوب ترتيب هذه القائمة تصاعديًا 
-ليكون المخرج كالتالي :
-#OUTPUT : ['HY0OH','IIUH1I','HKU3K','ER4TB','5JGU','I7YUI','KJI8TE']
-
-'''
